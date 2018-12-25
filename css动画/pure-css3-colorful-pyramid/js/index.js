d8105abdabcd8827f43ecd4af14889834838c469
leleke <asd_email@163.com>
2018-12-25 18:07:27 +0800

    init

---
+++ css动画/pure-css3-colorful-pyramid/js/index.js
@@ -0,0 +1,5 @@
+/* 
+This demo (and a lot of other recent 3D demos I've done) uses `clip-path` on HTML elements, currently not supported in IE/ Edge. 
+
+If you like my 3D stuff, please vote for `clip-path` on HTML elements to be implemented in Edge https://wpdev.uservoice.com/forums/257854-microsoft-edge-developer/suggestions/6519995-support-clip-path-property
+*/
